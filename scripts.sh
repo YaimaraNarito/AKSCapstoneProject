f06d4dae80db889eda5b1882dcbbe6d7cf89b22c
David Schepis <davidschepis@gmail.com>
2022-05-27 15:41:21 -0700

    added kube files for creating a deployment/ingress

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,29 @@
+##Just writing common commands here for easy copy/paste
+
+##Docker Commands
 docker build -t davidsdeveloper/llyd:v2 .
 
 docker run -it --name llyd -d -p 3001:3001 davidsdeveloper/llyd:v2
 
 docker push davidsdeveloper/llyd:v2
+
+##kubernetes commands
+az aks show --resource-group aks-rg1 --name aksdemo1 --query nodeResourceGroup -o tsv
+
+az network public-ip create --resource-group <REPLACE-OUTPUT-RG-FROM-PREVIOUS-COMMAND> --name myAKSPublicIPForIngress --sku Standard --allocation-method static --query publicIp.ipAddress -o tsv
+
+kubectl create namespace ingress-basic
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+    --namespace ingress-basic \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set controller.service.externalTrafficPolicy=Local \
+    --set controller.service.loadBalancerIP="REPLACE_STATIC_IP"
+
+kubectl apply -f kube-manifests/
+
