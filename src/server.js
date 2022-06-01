f4643de34044f50c46adb80896a712572379de61
YaimaraNarito <85261784+YaimaraNarito@users.noreply.github.com>
2022-06-01 14:51:50 -0500

    Deleted code block

--- src/server.js
+++ src/server.js
@@ -8,24 +8,20 @@ app.use(express.json());
 
 app.use(express.static("public/"));
 
-app.get("*/ping", (req, res) => {
+app.get("/ping", (req, res) => {
     console.info(`${req.method} request received, responding with pong.html`);
     res.sendFile(path.join(__dirname, "public/pong.html"));
 });
 
-app.get("*/version", (req, res) => {
+app.get("/version", (req, res) => {
     console.info(`${req.method} request received, responding with version.html`);
     res.sendFile(path.join(__dirname, "public/version.html"));
 });
 
-app.get("*/roman/:num", (req, res) => {
-    res.redirect(`/roman.html?num=${req.params.num}`)
-});
-
-app.get("*/:num", (req, res) => {
+app.get("/:num", (req, res) => {
     result = Number(req.params.num)
     if (isNaN(result)) {
-        res.sendFile(path.join(__dirname, "public/roman.html"));
+        res.sendFile(path.join(__dirname, "public/index.html"));
     }
     else {
         console.info(`${req.method} request received, responding with roman.html with param ${req.params.num}`);
